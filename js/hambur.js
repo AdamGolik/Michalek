8b1a474f47a06eb6f433978c40b43ea2d238b413
AdamGolik <156575468+AdamGolik@users.noreply.github.com>
2024-11-03 15:11:38 +0100

    Add files via upload

---
+++ js/hambur.js
@@ -0,0 +1,7 @@
+
+    function toggleMenu() {
+    const navItem = document.querySelector('.nav-item');
+    const hamburger = document.querySelector('.hamburger');
+    navItem.classList.toggle('active');
+    hamburger.classList.toggle('active');
+}
